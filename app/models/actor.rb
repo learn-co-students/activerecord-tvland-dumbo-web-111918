078816210a9b2117188f0924166143c81d06842f
github username <github email address>
2018-12-04 17:41:27 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+  	self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+  	characters.all.map do |char|
+      a = char.name + " - " + Show.find_by(id:char.show_id).name
+    end
+  end
 end
