ad82c101b1a19726fb9c68bf0b260c6a0a1a4df7
susan <bluenote@outlook.com>
2018-12-09 12:14:55 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+ has_many :characters
+ has_many :shows, through: :characters
+
+  def full_name
+   "#{first_name} #{last_name}"
+  end
+
+  #characters is join class so can call
+  #self.characters. Then can iterate through join to get
+  #character.name and also character.show.name (the show's name)
+  def list_roles
+  	self.characters.map do |character|
+  		"#{character.name} - #{character.show.name}"
+  	end
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,19 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+  #has_many :shows
+  #has_many :networks, through: :shows
+
+  def say_that_thing_you_say
+  	"#{self.name} always says: #{self.catchphrase}"
+  end
+
+#already in ActiveRecord. As a character can know about its show
+#thus it can know about its shows names.
+#And the show can call build network to learn about its network and
+#network call letters as they both have many
+#character can chain-build associations to which it belongs
+#malcolm.build_show(:name => "Firefly").build_network(:call_letters => "Fox")
+
+
 end
